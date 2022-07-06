efbe74ebb5ff18f319de4d4a183894e60d59af06
User <levomado123@gmail.com>
2022-07-06 11:05:44 +0300

    mnogodivo

---
+++ src/onetohundred.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class onetohundred {
+    public static void main(String[] args) {
+
+//          while (i<=99 ){
+//             //i++;
+//              i += 2;
+//
+//             System.out.println(i);
+        for (;;){
+
+        }
+
+
+    }
+
+}
