699d0f9f0dd89df99dfefb9e2a1728e64afd3f33
User <levomado123@gmail.com>
2022-07-07 10:33:56 +0300

    sada

--- src/onetohundred.java
+++ src/onetohundred.java
@@ -1,16 +1,35 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class onetohundred {
     public static void main(String[] args) {
 
-//          while (i<=99 ){
-//             //i++;
-//              i += 2;
-//
-//             System.out.println(i);
-        for (;;){
 
-        }
+           System.out.print("Enter first runners time: ");
+           Scanner in = new Scanner(System.in);
+           int firstRunner = in.nextInt();
+           System.out.print("Enter second runners time: ");
+           Scanner in1 = new Scanner(System.in);
+           int secondRunner = in.nextInt();
+           System.out.print("Enter third runners time: ");
+           Scanner in2 = new Scanner(System.in);
+           int thirdRunner = in.nextInt();
+           while ((firstRunner < 0 || firstRunner > 50) || (secondRunner < 0 || secondRunner > 50) || (thirdRunner < 0 || thirdRunner > 50))
+
+           {
+               System.out.println("Please enter runners time again ");
+               System.out.print("Enter first runners time: ");
+               firstRunner = in.nextInt();
+               System.out.print("Enter second runners time: ");
+               secondRunner = in.nextInt();
+               System.out.print("Enter third runners time: ");
+               thirdRunner = in.nextInt();
+
+           }
+           int secondsSum = firstRunner + secondRunner + thirdRunner;
+           int minutes = secondsSum / 60;
+           int seconds = secondsSum % 60;
+           System.out.print("The time is: " + minutes + ":" + seconds);
 
 
     }
